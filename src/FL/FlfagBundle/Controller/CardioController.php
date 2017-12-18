5edeec21dd534f498ab266b71da02250ec032a02
root <root@debian.nicolasbuton.com>
2017-12-18 15:27:43 +0100

    routing and views

--- src/FL/FlfagBundle/Controller/CardioController.php
+++ src/FL/FlfagBundle/Controller/CardioController.php
@@ -13,6 +13,26 @@ class CardioController extends Controller
   public function indexAction()
   {
     $em = $this->getDoctrine()->getManager();
+
     return $this->render('FLFlfagBundle:FLFAG:index.html.twig');
+  }
+
+  public function transitAction()
+  {
+    return $this->render('FLFlfagBundle:FLFAG:transit.html.twig');
+  }
+
+  public function toDoctorAction($patientId, $staffId)
+  {
+    return $this->render('FLFlfagBundle:FLFAG:doctor.html.twig',
+      array(
+        'patientId' => $patientId,
+        'staffId' => $staffId
+      ));
+  }
+
+  public function toStaffAction($patientId)
+  {
+    return $this->render('FLFlfagBundle:FLFAG:staff.html.twig');
   }
 }
