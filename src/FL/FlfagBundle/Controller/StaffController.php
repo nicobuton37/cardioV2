3d8ba5abd81dc4fd2ca4c7be93a518097c087f91
root <root@debian.nicolasbuton.com>
2017-12-27 18:20:41 +0100

    render datas in stff view

--- src/FL/FlfagBundle/Controller/StaffController.php
+++ src/FL/FlfagBundle/Controller/StaffController.php
@@ -16,6 +16,17 @@ class StaffController extends Controller
     public function indexAction($codePatient)
     {
         $patient = $this->getDoctrine()->getRepository("FLFlfagBundle:Patient")->findOneBy(array('codePatient' => $codePatient));
-        return $this->render("FLFlfagBundle:FLFAG/Staff:index.html.twig", array('codePatient' => $codePatient, 'patient' => $patient));
+        $doctor = $this->getDoctrine()->getRepository("FLFlfagBundle:Doctor")->findOneBy(array('id' => $patient->getDoctor()));
+        $traitement = $this->getDoctrine()->getRepository("FLFlfagBundle:Traitement")->findOneBy(array('id' => $patient->getTraitement()));
+        $cha = $this->getDoctrine()->getRepository("FLFlfagBundle:Cha")->findOneBy(array('id' => $patient->getCha()));
+        $has = $this->getDoctrine()->getRepository("FLFlfagBundle:Has")->findOneBy(array('id' => $patient->getHas()));
+        return $this->render("FLFlfagBundle:FLFAG/Staff:index.html.twig", array(
+            'codePatient' => $codePatient,
+            'patient' => $patient,
+            'doctor' => $doctor,
+            'traitement' => $traitement,
+            'cha' => $cha,
+            'has' => $has
+        ));
     }
 }
